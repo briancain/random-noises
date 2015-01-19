9d3829d483bb7476effdb59e37425e33fdd87a1a
Brian Cain <brianccain@gmail.com>
2015-01-18 21:06:27 -0800

    Init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to random-noises
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/random_noises/core_test.clj
@@ -0,0 +1,7 @@
+(ns random-noises.core-test
+  (:require [clojure.test :refer :all]
+            [random-noises.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
